fa1f4a239e61e83750bcf79a5d7ac194368cb598
dasbiplob <biplob90.d@gmail.com>
2025-08-08 15:42:08 +0300

    GHA cicd modified

---
+++ infra/main.tf
@@ -0,0 +1,133 @@
+resource "aws_lambda_function" "myfunc" {
+  filename         = data.archive_file.zip_the_python_code.output_path
+  source_code_hash = data.archive_file.zip_the_python_code.output_base64sha256
+  function_name    = "myfunc"
+  role             = aws_iam_role.iam_for_lambda.arn
+  handler          = "lambdaFunc.lambda_handler"
+  runtime          = "python3.8"
+
+  environment {
+    variables = {
+      TABLE_NAME = aws_dynamodb_table.cloudresume.name
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "cloudresume" {
+  name         = "clould-portfolio-test"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  tags = {
+    Name = "clould-portfolio-test"
+  }
+}
+
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = "cloud-portfolio-biplob"
+}
+
+resource "aws_s3_bucket_public_access_block" "website_bucket" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "public_read" {
+  bucket = aws_s3_bucket.website_bucket.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = "*",
+        Action = "s3:GetObject",
+        Resource = "${aws_s3_bucket.website_bucket.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "iam_policy_for_resume_project" {
+
+  name        = "aws_iam_policy_for_terraform_resume_project_policy"
+  path        = "/"
+  description = "AWS IAM Policy for managing the resume project role"
+    policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Action" : [
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents"
+          ],
+          "Resource" : "arn:aws:logs:*:*:*",
+          "Effect" : "Allow"
+        },
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "dynamodb:UpdateItem",
+			      "dynamodb:GetItem"
+          ],
+          "Resource" : "arn:aws:dynamodb:*:*:table/clould-portfolio-test"
+        },
+      ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
+  role = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.iam_policy_for_resume_project.arn
+  
+}
+
+data "archive_file" "zip_the_python_code" {
+  type        = "zip"
+  source_file = "${path.module}/lambda/lambdaFunc.py"
+  output_path = "${path.module}/lambda/lambdaFunc.zip"
+}
+
+resource "aws_lambda_function_url" "url1" {
+  function_name      = aws_lambda_function.myfunc.function_name
+  authorization_type = "NONE"
+
+  cors {
+    allow_credentials = true
+    allow_origins     = ["*"]
+    allow_methods     = ["*"]
+    allow_headers     = ["date", "keep-alive"]
+    expose_headers    = ["keep-alive", "date"]
+    max_age           = 86400
+  }
+}
